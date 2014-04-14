f1cde05222aec46ca412cfc4d1d387882301a3a5
Guz Alexander <kalimatas@gmail.com>
2014-04-14 13:12:58 +0400

    Parse command line arguments

--- README.md
+++ README.md
@@ -9,8 +9,19 @@ git clone git@github.com:kalimatas/githubissues.git
 cd githubissues
 git submodule init
 git submodule update
-export $GOPATH=$(pwd)
+export GOPATH=$(pwd)
 go get -u github.com/google/go-querystring/query
 go get -u code.google.com/p/goauth2/oauth
-go install ./...
+go install github.com/kalimatas/githubissues
+```
+
+### Usage
+
+```
+Usage of githubissues:
+  -access-token="": access token for authenticated access
+  -issues="": comma-separated issue numbers list
+  -milestone=0: milestone number
+  -owner="": repository owner
+  -repository="": repository name
 ```
--- src/github.com/kalimatas/githubissues/githubissues.go
+++ src/github.com/kalimatas/githubissues/githubissues.go
@@ -1,10 +1,60 @@
+// Generate HTML for Github issues
+
 package main
 
 import (
 	"fmt"
+	"os"
+	"flag"
+	"strings"
+	"strconv"
+
+	_"github.com/google/go-github/github"
 )
 
-func main() {
-	fmt.Println("hello")
+const (
+	issuesSeparator = ","
+)
+
+// Command-line arguments
+var owner string
+var repository string
+var accessToken string
+var milestone int
+var issues string
+
+func init() {
+	flag.StringVar(&owner, "owner", "", "repository owner")
+	flag.StringVar(&repository, "repository", "", "repository name")
+	flag.StringVar(&accessToken, "access-token", "", "access token for authenticated access")
+	flag.IntVar(&milestone, "milestone", 0, "milestone number")
+	flag.StringVar(&issues, "issues", "", "comma-separated issue numbers list")
 }
 
+func main() {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintf(os.Stderr, "error: %v", r)
+		}
+	}()
+
+	flag.Parse()
+
+	if milestone == 0 && issues == "" {
+		fmt.Fprint(os.Stderr, "Error: either milestone or issues must be set\n\n")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	var issueList []int
+	if (issues != "") {
+		for _, numberStr := range strings.Split(issues, issuesSeparator) {
+			number, err := strconv.Atoi(numberStr)
+			if err == nil {
+				issueList = append(issueList, number)
+			} else {
+				panic(fmt.Sprintf("%v is not an issue number", numberStr))
+			}
+		}
+	}
+}
