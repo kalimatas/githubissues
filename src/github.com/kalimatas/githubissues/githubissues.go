7146f87fc1e22209163d3d43bca79fec5a855ee7
Guz Alexander <kalimatas@gmail.com>
2014-04-14 17:02:09 +0400

    fetchIssues

--- src/github.com/kalimatas/githubissues/githubissues.go
+++ src/github.com/kalimatas/githubissues/githubissues.go
@@ -3,14 +3,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"flag"
-	"strings"
 	"strconv"
+	"strings"
 
-	"github.com/google/go-github/github"
 	"code.google.com/p/goauth2/oauth"
+	"github.com/google/go-github/github"
 )
 
 const (
@@ -21,14 +21,46 @@ const (
 var owner string
 var repository string
 var accessToken string
-var milestone int
+var milestone string
 var issues string
+
+type IssueManager struct {
+	client     *github.Client
+	owner      string
+	repository string
+}
+
+// fetchIssues fetches issues by milestone from Github tracker
+func (m *IssueManager) fetchByMilestone(milestone string) ([]github.Issue, error) {
+	issues, _, err := m.client.Issues.ListByRepo(m.owner, m.repository, &github.IssueListByRepoOptions{Milestone: milestone})
+	if err != nil {
+		return nil, err
+	}
+
+	return issues, err
+}
+
+// fetchIssues fetches issues by numbers from Github tracker
+func (m *IssueManager) fetchByNumbers(issueList []int) ([]github.Issue, error) {
+	var issues []github.Issue
+
+	for _, issueNumber := range issueList {
+		issue, _, err := m.client.Issues.Get(m.owner, m.repository, int(issueNumber))
+		if err != nil {
+			return issues, err
+		}
+
+		issues = append(issues, *issue)
+	}
+
+	return issues, nil
+}
 
 // newClient returns *github.Client
 //
 // With empty accessToken a client with not authorized access is created (for public repos).
 func newClient(accessToken string) (client *github.Client) {
-	if (accessToken == "") {
+	if accessToken == "" {
 		client = github.NewClient(nil)
 	} else {
 		transport := &oauth.Transport{
@@ -44,7 +76,7 @@ func init() {
 	flag.StringVar(&owner, "owner", "", "repository owner")
 	flag.StringVar(&repository, "repository", "", "repository name")
 	flag.StringVar(&accessToken, "access-token", "", "access token for authenticated access")
-	flag.IntVar(&milestone, "milestone", 0, "milestone number")
+	flag.StringVar(&milestone, "milestone", "", "milestone number")
 	flag.StringVar(&issues, "issues", "", "comma-separated issue numbers list")
 }
 
@@ -57,14 +89,14 @@ func main() {
 
 	flag.Parse()
 
-	if milestone == 0 && issues == "" {
+	if milestone == "" && issues == "" {
 		fmt.Fprint(os.Stderr, "Error: either milestone or issues must be set\n\n")
 		flag.Usage()
 		os.Exit(2)
 	}
 
 	var issueList []int
-	if (issues != "") {
+	if issues != "" {
 		for _, numberStr := range strings.Split(issues, issuesSeparator) {
 			number, err := strconv.Atoi(numberStr)
 			if err == nil {
@@ -75,5 +107,21 @@ func main() {
 		}
 	}
 
-	_ := newClient(accessToken)
+	manager := &IssueManager{
+		client: newClient(accessToken),
+		owner: owner,
+		repository: repository,
+	}
+
+	var issues []github.Issue
+	var fetchError error
+	if len(issueList) > 0 {
+		issues, fetchError = manager.fetchByNumbers(issueList)
+	} else {
+		issues, fetchError = manager.fetchByMilestone(milestone)
+	}
+
+	if fetchError != nil {
+		panic(fmt.Sprintf("cannot fetch issues: %v", fetchError))
+	}
 }
